26d514cdae41f8e10ade5ab247a3630cb332083b
tamaki1121 <koizumi88ohara@gmail.com>
2021-02-09 12:38:24 +0900

    edit token

--- purchase_input.php
+++ purchase_input.php
@@ -11,21 +11,24 @@
 <body>
 
 	<?php require 'menu.php'; ?>
-	<?php if (!empty($_SESSION['product'])) {
+	<?php if (!empty($_SESSION['customer'])) {
 
-		// if (!empty($_SESSION['customer'])) {
 		printf(
 			"氏名 : %s様<br/>お届け先 : %s<br/>",
 			$_SESSION['customer']['name'],
 			$_SESSION['customer']['address']
 		);
 		require 'cart.php';
-		print('<a href="purchase_output.php">購入を確定する</a><br/>');
-		// } else {
-		print("ログインしてください。<br/>");
-		// }
+		if (!empty($_SESSION['product'])) {
+			$_SESSION['token'] = random_int(1, 1000000); ?>
+			<form id="buy" action="purchase_output.php" method="POST">
+				<input type="text" name="token" value="<?= $_SESSION['token'] ?>">
+				<input type="submit" value="購入を確定する"><br />
+			</form>
+	<?php
+		};
 	} else {
-		print('カートに商品がありません。<br/>');
+		print("ログインしてください。<br/>");
 	}
 	?>
 
--- purchase_output.php
+++ purchase_output.php
@@ -14,6 +14,23 @@
 	$pdo;
 	require 'db_connect.php';
 	?>
+	<?php
+	if (!empty($_SESSION['customer']) && !empty($_SESSION['product'])) {
+		if (isset($_POST['token']) && !empty($_SESSION['token'])) {
+			if ($_POST['token'] == $_SESSION['token']) {
+				$_SESSION['token'] = NULL;
+				echo "処理をします";
+			} else {
+				echo $_SESSION['token'] . "不正なトークンです";
+				echo $_POST['token'] . "購入ページからアクセスしてください。<br/>";
+			}
+		} else {
+			echo "購入ページからアクセスしてください。<br/>";
+		}
+	} else {
+		if (empty($_SESSION['customer'])) echo "ログインしてください。<br/>";
+		if (empty($_SESSION['product'])) echo "ログインしてください。<br/>";
+	} ?>
 
 </body>
 
